81c75b92d68a1ec92b82f393d784488b2703d1e7
xlh-fm <416311366@qq.com>
2019-05-01 16:23:47 +0900

    test scan by rowkey

--- consumer/src/test/java/RowKeyScanTest.java
+++ consumer/src/test/java/RowKeyScanTest.java
@@ -1,6 +1,33 @@
+import com.xu.utils.HBaseRowKeyScanUtil;
+import com.xu.utils.HBaseUtil;
+import com.xu.utils.ResourcesUtil;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
+
 public class RowKeyScanTest {
     //09078923434,2018,04-06
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException, IOException {
 
+        //get every startrow and stoprow
+        List<String[]> startStops = HBaseRowKeyScanUtil.getStartStop("09078923434", "2018-11", "2019-01");
+
+        Connection connection = ConnectionFactory.createConnection(HBaseUtil.configuration);
+        Table table = connection.getTable(TableName.valueOf(ResourcesUtil.getProperties().getProperty("hbase.table" +
+                ".name")));
+
+        for (String[] startStop : startStops) {
+            Scan scan = new Scan(Bytes.toBytes(startStop[0]), Bytes.toBytes(startStop[1]));
+            System.out.println(startStop[0]);
+            System.out.println(startStop[1]);
+            ResultScanner scanner = table.getScanner(scan);
+            for (Result result : scanner) {
+                System.out.println(Bytes.toString(result.getRow()));
+            }
+        }
     }
 }
