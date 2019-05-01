2236352f0a7b11f27dc316197165d67ba75c9e69
xlh-fm <416311366@qq.com>
2019-05-01 23:07:39 +0900

    create duration value

---
+++ analysis/src/main/java/com/xu/kv/CalculateDurationValue.java
@@ -0,0 +1,51 @@
+package com.xu.kv;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class CalculateDurationValue extends Value {
+    private int count;
+    private int duration;
+
+    public CalculateDurationValue() {
+    }
+
+    public CalculateDurationValue(int count, int duration) {
+        this.count = count;
+        this.duration = duration;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return count + "\t" + duration;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeInt(count);
+        dataOutput.writeInt(duration);
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        count = dataInput.readInt();
+        duration = dataInput.readInt();
+    }
+}
