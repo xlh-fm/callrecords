5d8b68572dbd7192e78ee95576db23560c3c7d47
xlh-fm <416311366@qq.com>
2019-05-03 16:27:13 +0900

    create jdbc utils for get and close connection

---
+++ analysis/src/main/java/com/xu/utils/JDBCUtil.java
@@ -0,0 +1,60 @@
+package com.xu.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
+public class JDBCUtil {
+    private static Properties properties = null;
+    private static Connection connection = null;
+
+    static {
+        InputStream resourceAsStream = ClassLoader.getSystemResourceAsStream("jdbc.properties");
+        properties = new Properties();
+        try {
+            properties.load(resourceAsStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName(properties.getProperty("MYSQL_DRIVER_CLASS"));
+        return DriverManager.getConnection(properties.getProperty("MYSQL_URL"), properties.getProperty("MYSQL_USER"),
+                properties.getProperty("MYSQL_PASSWORD"));
+    }
+
+    public static Connection getInstance() throws SQLException, ClassNotFoundException {
+        if (connection == null) {
+            connection = getConnection();
+        }
+        return connection;
+    }
+
+    public static void close(Connection connection, Statement statement, ResultSet resultSet) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
